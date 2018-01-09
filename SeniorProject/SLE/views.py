08676636d07ed61f1641876d34c295e942269461
u5788217 <5788217k@gmail.com>
2018-01-09 20:34:10 +0700

    Insert some data to database

--- SeniorProject/SLE/views.py
+++ SeniorProject/SLE/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Studyidentity
-from .models import Slicccriteria
-from .models import Medicalcondition
-from .models import Acrcriteria
+#Models for enrollment
+from .models import Studyidentity, Slicccriteria, Acrcriteria, Medicalcondition
 
 def login(request):
     return render(request, 'login.html')
 
 def index(request):
+    #if request.method == "POST": return username
     return render(request, 'index.html')
 
 def logout(request):
@@ -31,37 +30,53 @@ def enrollAdd(request):
 def enrollPatient(request):
     if request.method == "POST":
         #All data feilds
-  #      EnrollStudyidentity = Studyidentity(dateofdiagnosis = request.POST.get('dateofdiagnosis', ''),
-  #                              dateofenrollment = request.POST.get('dateofenrollment', ''),
-  #                              gender = request.POST.get('gender', ''),
-  #                              dateofbirth = request.POST.get('dateofbirth', ''),
-  #                              religion = request.POST.get('religion', ''),
-  ##                              education = request.POST.get('education', ''),
-  #                              maritalstatus = request.POST.get('maritalstatus', ''),
-  #                              region = request.POST.get('region', ''),
-   #                             occupation = request.POST.get('occupation', ''),
-   #                             income = request.POST.get('income', ''))
-        #print(EnrollStudyidentity)
-        #EnrollStudyidentity.save()
-        #StudyNumber = EnrollStudyidentity.studynumber
-        EnrollSlicccriteria = Slicccriteria(slicc1 = request.POST.get('slicc1', ''),
-                                            slicc2 = request.POST.get('slicc2', ''),
-                                            slicc3 = request.POST.get('slicc3', ''),
-                                            slicc4 = request.POST.get('slicc4', ''),
-                                            slicc5 = request.POST.get('slicc5', ''),
-                                            slicc6 = request.POST.get('slicc6', ''),
-                                            slicc7 = request.POST.get('slicc7', ''),
-                                            slicc8 = request.POST.get('slicc8', ''),
-                                            slicc9 = request.POST.get('slicc9', ''),
-                                            slicc10 = request.POST.get('slicc10', ''),
-                                            slicc11 = request.POST.get('slicc11', ''),
-                                            slicc12 = request.POST.get('slicc12', ''),
-                                            slicc13 = request.POST.get('slicc13', ''),
-                                            slicc14 = request.POST.get('slicc14', ''),
-                                            slicc15 = request.POST.get('slicc15', ''),
-                                            slicc16 = request.POST.get('slicc16', ''),
-                                            slicc17 = request.POST.get('slicc17', ''))
-        #EnrollAcrcriteria = Acrcriteria()
+        #create studynumber+1
+        stnum = 2
+        EnrollStudyidentity = Studyidentity(studynumber = stnum,
+                                dateofdiagnosis = request.POST.get('dateofdiagnosis', ''),
+                                dateofenrollment = request.POST.get('dateofenrollment', ''),
+                                gender = request.POST.get('gender', ''),
+                                dateofbirth = request.POST.get('dateofbirth', ''),
+                                religion = request.POST.get('religion', ''),
+                                education = request.POST.get('education', ''),
+                                maritalstatus = request.POST.get('maritalstatus', ''),
+                                region = request.POST.get('region', ''),
+                                occupation = request.POST.get('occupation', ''),
+                                income = request.POST.get('income', ''))
+        EnrollStudyidentity.save()
+        
+        EnrollSlicccriteria = Slicccriteria(studynumber = EnrollStudyidentity,
+                                            slicc1 = 'True' if request.POST.get('slicc1', '') == '1' else 'False',
+                                            slicc2 = 'True' if request.POST.get('slicc2', '') == '1' else 'False',
+                                            slicc3 = 'True' if request.POST.get('slicc3', '') == '1' else 'False',
+                                            slicc4 = 'True' if request.POST.get('slicc4', '') == '1' else 'False',
+                                            slicc5 = 'True' if request.POST.get('slicc5', '') == '1' else 'False',
+                                            slicc6 = 'True' if request.POST.get('slicc6', '') == '1' else 'False',
+                                            slicc7 = 'True' if request.POST.get('slicc7', '') == '1' else 'False',
+                                            slicc8 = 'True' if request.POST.get('slicc8', '') == '1' else 'False',
+                                            slicc9 = 'True' if request.POST.get('slicc9', '') == '1' else 'False',
+                                            slicc10 = 'True' if request.POST.get('slicc10', '') == '1' else 'False',
+                                            slicc11 = 'True' if request.POST.get('slicc11', '') == '1' else 'False',
+                                            slicc12 = 'True' if request.POST.get('slicc12', '') == '1' else 'False',
+                                            slicc13 = 'True' if request.POST.get('slicc13', '') == '1' else 'False',
+                                            slicc14 = 'True' if request.POST.get('slicc14', '') == '1' else 'False',
+                                            slicc15 = 'True' if request.POST.get('slicc15', '') == '1' else 'False',
+                                            slicc16 = 'True' if request.POST.get('slicc16', '') == '1' else 'False',
+                                            slicc17 = 'True' if request.POST.get('slicc17', '') == '1' else 'False')
+        EnrollSlicccriteria.save()
+        EnrollAcrcriteria = Acrcriteria(studynumber = EnrollStudyidentity,
+                                        acr1 = 'True' if request.POST.get('acr1', '')== '1' else 'False',
+                                        acr2 = 'True' if request.POST.get('acr2', '')== '1' else 'False',
+                                        acr3 = 'True' if request.POST.get('acr3', '')== '1' else 'False',
+                                        acr4 = 'True' if request.POST.get('acr4', '')== '1' else 'False',
+                                        acr5 = 'True' if request.POST.get('acr5', '')== '1' else 'False',
+                                        acr6 = 'True' if request.POST.get('acr6', '')== '1' else 'False',
+                                        acr7 = 'True' if request.POST.get('acr7', '')== '1' else 'False',
+                                        acr8 = 'True' if request.POST.get('acr8', '')== '1' else 'False',
+                                        acr9 = 'True' if request.POST.get('acr9', '')== '1' else 'False',
+                                        acr10 = 'True' if request.POST.get('acr10', '')== '1' else 'False',
+                                        acr11 = 'True' if request.POST.get('acr11', '')== '1' else 'False')
+        EnrollAcrcriteria.save()
         #EnrollMedicalcondition = Medicalcondition()
     return render(request, 'enrollment-detail.html',{'EnrollSlicccriteria': EnrollSlicccriteria.slicc1})
 
