da23eef8b8a13b434a24e3fcc9d5e69e338b939a
Supatjana Leardarpajit <suusuusuu00@gmail.com>
2018-05-15 17:33:02 +0700

    จ้าก

--- SeniorProject/SLE/admin.py
+++ SeniorProject/SLE/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import AuthUser
-
-class PersonAdmin(admin.ModelAdmin):
-    exclude = ('email', 'date_joined', )
-
-admin.site.register(AuthUser, PersonAdmin)
-    
